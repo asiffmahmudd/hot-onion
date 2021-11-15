18b086f8581f643fb563b1fe47fa70e5490e581c
Asif Mahmud <asifmahmud3472@gmail.com>
2021-11-15 10:48:52 -0500

    added react bootstrap in header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,35 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import logo from '../../images/logo2.png';
 import './Header.css';
 import { AiOutlineShoppingCart } from "react-icons/ai";
-
+import { Container, Nav, Navbar } from 'react-bootstrap';
 const Header = () => {
     return (
         <header>
-            <div className="container-fluid">
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <Link className="navbar-brand" to="/">
+            <Navbar expand="md">
+                <Container fluid>
+                    <Navbar.Brand href="/">
                         <img className="logo" src={logo} alt="logo"/>
-                    </Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav
+                            className="my-2 my-lg-0"
+                        >
+                            <Nav.Link c>
                                 <span className="cart-icon cursor-pointer"><AiOutlineShoppingCart size={30} /></span>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link btn login-btn" to="/login">Login</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link btn signup-btn" to="/signUp">Sign Up</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+                            </Nav.Link>
+                            <Nav.Link className="btn login-btn" href="/login">Login</Nav.Link>
+                            <Nav.Link className="btn signup-btn" href="/signUp">Sign Up</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     );
 };
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../common/Header';
 import Banner from './Banner/Banner';
+import Options from './Options/Options';
 
 const Home = () => {
     return (
         <>
             <Header />
             <Banner />
+            <Options />
         </>
     );
 };
